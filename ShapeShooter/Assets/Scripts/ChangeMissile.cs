9e1534cfbce31e9f031d05975098ef3472dad6fd
Michael Capra (RIT Student) <mcc2337@ad.rit.edu>
2019-10-09 15:51:03 -0400

    I have a UI element that displays the current missle. The missle does in fact know where it is.

---
+++ ShapeShooter/Assets/Scripts/ChangeMissile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMissile : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> missiles; //List of different Missiles.
+
+    public int curMissileInd; //Index of the Current Missile.
+
+
+    public Camera camera; //For making it look not shit
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        curMissileInd = 0;
+
+        missiles[curMissileInd].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mouseScrollDelta.y * 0.1f);
+
+        TurnOffShape();
+
+        curMissileInd += (int)Input.mouseScrollDelta.y;
+
+        if(curMissileInd > 2)
+        {
+            curMissileInd = 0;
+        }
+
+        if(curMissileInd < 0)
+        {
+            curMissileInd = 2;
+        }
+
+        missiles[curMissileInd].SetActive(true);
+        missiles[curMissileInd].transform.LookAt(camera.transform);
+    }
+
+    void TurnOffShape()
+    {
+        missiles[curMissileInd].SetActive(false);
+    }
+}
